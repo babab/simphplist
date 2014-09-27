227c31ae94b875df3aa96b446204e2ebed7a2458
Benjamin Althues <benjamin@babab.nl>
2014-09-28 01:31:40 +0200

    Add new Route component and example

---
+++ examples/public/route.php
@@ -0,0 +1,37 @@
+<?php
+function manual_load()
+{
+    $d = dirname(dirname(__DIR__));
+    require $d . "/src/Babab/Simphplist/Route.php";
+}
+(@include dirname(__DIR__) . "/vendor/autoload.php") || manual_load();
+
+##############################################################################
+
+(new \Babab\Simphplist\Route)
+
+->setPrefix('/route.php')
+
+->when('/articles/archive/{year}/{month}/', function($args) {
+
+    echo '<h1>Archives: year "' . $args->year . '"</h1>';
+    echo '<h2>Month "' . $args->month . '"</h2>';
+
+})
+->when('/articles/{id}/', function($args) {
+
+    echo '<h1>Welcome to article "' . $args->id . '"</h1>';
+
+})
+->when('/articles/', function($args) {
+
+    echo '<h1>Welcome to the article list</h1>';
+
+})
+->other(function() {
+
+    echo '<h1>No other matches found, this could be a 404 page</h1>';
+    // or a redirect
+    // \Babab\Simphplist\Route::redirect('/route.php/articles/');
+
+});
---
+++ src/Babab/Simphplist/Route.php
@@ -0,0 +1,191 @@
+<?php
+/*
+ * Simphplist Route
+ *
+ * Copyright (c) 2014 Benjamin Althues <benjamin@babab.nl>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+namespace Babab\Simphplist;
+
+/**
+ * @class Route
+ * @brief Flexible route handling
+ */
+
+class Route {
+
+    public static function redirect($uri, $httpPrefixer = false)
+    {
+        if ($httpPrefixer && strpos($uri, 'http') === false)
+            $uri = 'http://' . $uri;
+        header("Location: $uri");
+        exit;
+    }
+
+    private $_prefix;
+
+    // Set to true when a when match is found to stop routing
+    private $_matched = false;
+
+    /**
+     * Run a default closure when no other previous `when()` calls
+     * have matched. The closure cannot accept arguments.
+     *
+     * @param callable $func A closure or variable function to run
+     * @retval bool | array Match success or matched identifier pair
+     */
+    public function other($func)
+    {
+        if ($this->_matched)
+            return $this;
+
+        $func();
+        $this->_matched = true;
+        return $this;
+    }
+
+    /**
+     * Run a closure when the visited URL matches the defined URI format
+     *
+     * An $uri can have identifiers, which are marked with `{}`.
+     * In the following example there is an 'id' identifier
+     * for a blog article:
+     *
+     *     (new \Babab\Simphplist\Routing\Route)
+     *
+     *     ->when('/blog/articles/{id}/, function($someArray) {
+     *          echo 'This is article: ' . $someArray['id'];
+     *     })
+     *
+     *     ->when('/blog/articles/, function($void) {
+     *          echo 'This is the article list';
+     *     })
+     *
+     *     ->other(function() {
+     *         \Babab\Simphplist\Route::redirect('/blog/articles/');
+     *     });
+     *
+     * When the url is matched, any values that are matched with
+     * identifiers are added to an object (with the identifiers as
+     * property names), which is in turn passed to the closure.
+     *
+     * The closure should therefore always accept an (object as)
+     * argument.
+     *
+     * @param string $uri The URI format to match for
+     * @param ... optional arguments to pass to closure (N/A yet)
+     * @param callable $func A closure or variable function to run
+     * @retval bool | array Match success or matched identifier pair
+     */
+    public function when()
+    {
+        if ($this->_matched)
+            return $this;
+
+        // Check for arguments and load them, argument 1
+        // should always be the path
+        if (!$n_args = func_num_args())
+            return $this;
+
+        $a_args = func_get_args();
+        $path = $a_args[0];
+
+        if (!is_string($path))
+            return $this;
+
+        // parse path and save closure arguments in $match
+        if (!$match = $this->parseURI($path))
+            return $this;
+
+        // if match is true it means there are no arguments but there
+        // still is a valid match; make sure to pass an array to the
+        // closure
+        if ($match === true)
+            $match = [];
+
+        // When this point has been reached, there seems to be a match,
+        // any subsequent calls should not have any effect.
+        $this->_matched = true;
+
+        // add optional arguments to the closure and ultimately call
+        // when a callable is found (which can also be a predefined function
+        for ($i = 1; $i < $n_args; $i++) {
+            // the closure should always be last
+            if (is_callable($a_args[$i])) {
+                $a_args[$i]($match);
+                return $this;
+            }
+        }
+    }
+
+    public function setPrefix($prefix)
+    {
+        $this->_prefix = $prefix;
+        return $this;
+    }
+
+    /**
+     * Match REQUEST_URI with $reference path
+     *
+     * The REQUEST_URI is optionally stripped with prefix before
+     * matching (useful for developing without rewriting rules, with
+     * PHP's built in webserver for example)
+     *
+     * Returns an object with matched identifier pairs when applicable;
+     * Returns true then a match is found without identifiers;
+     * Returns false when no match is found
+     *
+     * @param string $referencePath The URI format to match for
+     * @retval bool | array Match success or matched identifier pair
+     */
+    public function parseURI($referencePath)
+    {
+        $uri = htmlentities($_SERVER['REQUEST_URI']);
+
+        if ($pln = strlen($this->_prefix))
+            $uri = substr($uri, $pln);
+
+        $uri = explode('/', $uri);
+        $referencePathComponents = explode('/', $referencePath);
+
+        // stop if the number of /'s do not match
+        if (count($uri) != count($referencePathComponents))
+            return false;
+
+        // parse referencePath and add identifier matches to closure_args
+        $i = 0;
+        $closure_args = new \stdClass;
+        foreach ($referencePathComponents as $p) {
+            if (!isset($uri[$i]))
+                return false;
+
+            $d1 = strpos($p, '{');
+            $d2 = strpos($p, '}');
+
+            if ($d1 !== false && $d2 !== false) {
+                // add to closure
+                $value = filter_var($uri[$i], FILTER_SANITIZE_STRING);
+                $closure_args->{substr($p, 1, $d2 - 1)} = $value;
+            }
+            else {
+                // continue on string match, else end it here
+                if ($p !== $uri[$i])
+                    return false;
+            }
+            $i++;
+        }
+        return $closure_args ?: true;
+    }
+}
